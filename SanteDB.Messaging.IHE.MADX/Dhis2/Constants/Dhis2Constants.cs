ac4553578718123a298446e6e14aa20cd6fb74f2
Jordan Webber <webberjo@hhsc.ca>
2025-09-10 10:48:55 -0400

    JIMS-892 Created constants file for DHIS2 values

---
+++ SanteDB.Messaging.IHE.MADX/Dhis2/Constants/Dhis2Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanteDB.Messaging.IHE.MADX.Dhis2.Constants
+{
+    public static class Dhis2Constants
+    {
+        public const string QueueName = "sys.dhis2";
+    }
+}
